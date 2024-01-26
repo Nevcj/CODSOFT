a667aae362981f357b3591a4f9f5deb9e49e095e
neevass07@gmail.com <neevass15@gmail.com>
2024-01-26 12:17:08 +0530

    added task1 file

--- Python_Programming/calculator.py
+++ Python_Programming/calculator.py
@@ -8,51 +8,41 @@ def multiply(x, y):
     return x * y
 
 def divide(x, y):
-    if y == 0:
-        return "Cannot Divide by Zero"
-    return x / y
+    if y != 0:
+        return x / y
+    else:
+        raise ValueError("Cannot Divide by Zero")
 
 print("=== Calculator ===")
+print("1) Addition")
+print("2) Subtraction")
+print("3) Multiplication")
+print("4) Division")
+print("5) Exit")
 
 while True:
     try:
-        num1 = float(input("\nEnter the first number: "))
+        choice = int(input("\nEnter your choice: "))
+        
+        if choice == 5:
+            break
+
+        if choice not in range(1, 5):
+            raise ValueError("Invalid Choice")
+
+        num1 = float(input("Enter the first number: "))
         num2 = float(input("Enter the second number: "))
-    except ValueError:
-        print("Invalid Input")
-        continue
 
-     
-    print("\n1) Addition")
-    print("2) Subtraction")
-    print("3) Multiplication")
-    print("4) Division")
-    print("5) Exit")
+        if choice == 1:
+            result = add(num1, num2)
+        elif choice == 2:
+            result = subtract(num1, num2)
+        elif choice == 3:
+            result = multiply(num1, num2)
+        elif choice == 4:
+            result = divide(num1, num2)
 
-    try:
-        choice = int(input("Enter your choice: "))
-    except ValueError:
-        print("Invalid Input")
-        continue
+        print(f"\nResult: {result}")
 
-    if choice == 5:
-        break
-
-    if choice not in range(1, 5):
-        print("Invalid Choice")
-        continue
-
-    if choice == 1:
-        result = add(num1, num2)
-        operation = "Addition"
-    elif choice == 2:
-        result = subtract(num1, num2)
-        operation = "Subtraction"
-    elif choice == 3:
-        result = multiply(num1, num2)
-        operation = "Multiplication"
-    elif choice == 4:
-        result = divide(num1, num2)
-        operation = "Division"
-
-    print(f"\nResult of {operation}: {result}")
+    except ValueError as e:
+        print(f"Error: {e}")
